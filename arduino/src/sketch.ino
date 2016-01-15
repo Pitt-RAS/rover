eb36b1f997345808c712b58b94937906e2ee514a
JDM152 <JDM152@pitt.edu>
2016-01-14 22:19:47 -0500

    Rewrote Arduino sketchup.ino
    Not yet tested.

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -1,44 +1,105 @@
 
-char buf[200];
-int chars_in_buf = 0;
+#define INPUT_BUFFER_SIZE = 200
+#define PARAMETER_SEPERATOR = ','
 
-int motor_fl_pwm_pin = 0;
-int motor_fr_pwm_pin = 0;
-int motor_bl_pwm_pin = 0;
-int motor_br_pwm_pin = 0;
-int motor_fl_gpio_pin = 0;
-int motor_fr_gpio_pin = 0;
-int motor_bl_gpio_pin = 0;
-int motor_br_gpio_pin = 0;
+#define MOTOR_FL_PWM_PIN = 0
+#define MOTOR_FR_PWM_PIN = 0
+#define MOTOR_BL_PWM_PIN = 0
+#define MOTOR_BR_PWM_PIN = 0
+#define MOTOR_FL_GPIO_PIN = 0
+#define MOTOR_FR_GPIO_PIN = 0
+#define MOTOR_BL_GPIO_PIN = 0
+#define MOTOR_BR_GPIO_PIN = 0
 
-void setup()
-{
+char inputBuffer[INPUT_BUFFER_SIZE];
+byte inputBufferSize = 0;
+
+// Cut down on some conditionals by picking from an array
+const byte[] motorPins = {
+  MOTOR_FL_PWM_PIN, MOTOR_FR_PWM_PIN, MOTOR_BL_PWM_PIN, MOTOR_BR_PWM_PIN,
+  MOTOR_FL_GPIO_PIN, MOTOR_FR_GPIO_PIN, MOTOR_BL_GPIO_PIN, MOTOR_BR_GPIO_PIN
+};
+
+//----------------------------------------------------------------------------
+// setup
+// Runs once when the program starts
+//----------------------------------------------------------------------------
+void setup(){
   Serial.begin(115200);
   Serial.setTimeout(10);
 }
 
-void loop()
-{
-  int new_chars = Serial.readBytesUntil('\n', buf + chars_in_buf, sizeof(buf) - chars_in_buf);
-  chars_in_buf += new_chars;
-  if (new_chars > 0 && buf[chars_in_buf - 1] == '\n') {
-    // process command
-    if (strncmp(buf, "motor", 5) == 0) {
-      float motor_speed = atof(buf + 9);
-      if (strncmp(buf + 6, "fl", 2) == 0) {
-        write_motor_speed(motor_speed, motor_fl_pwm_pin, motor_fl_gpio_pin);
-      } else if (strncmp(buf + 6, "fr", 2) == 0) {
-        write_motor_speed(motor_speed, motor_fr_pwm_pin, motor_fr_gpio_pin);
-      } else if (strncmp(buf + 6, "bl", 2) == 0) {
-        write_motor_speed(motor_speed, motor_bl_pwm_pin, motor_bl_gpio_pin);
-      } else if (strncmp(buf + 6, "br", 2) == 0) {
-        write_motor_speed(motor_speed, motor_br_pwm_pin, motor_br_gpio_pin);
-      }
-    }
+//----------------------------------------------------------------------------
+// loop
+// Main program loop
+//----------------------------------------------------------------------------
+void loop(){
+  byte newChars = Serial.readBytesUntil('\n', inputBuffer + inputBufferSize, sizeof(inputBuffer) - inputBufferSize);
+  inputBufferSize += newChars;
+  if (inputBufferSize > 0 && inputBffer[inputBufferSize - 1] == '\n'){
+    // Split the command, clear the buffer
+    char[][] command = splitString(inputBuffer,',');
+    inputBuffer = new char[INPUT_BUFFER_SIZE];
+    // Process the command
+    processCommand(command);
   }
 }
 
-void write_motor_speed(float speed, int pwm_pin, int gpio_pin) {
+//----------------------------------------------------------------------------
+// processCommand
+// Switch off to the different specified functions
+//----------------------------------------------------------------------------
+void processCommand(string[][] command){
+  switch (command[0][0]){
+    case 'm' : // Motor Command
+      int motorPinSet = 2 * (command[2][0] == 'f' : 0 ? 1) + (command[2][1] == 'l' : 0 ? 1);
+      writeMotorSpeed(atof(command[1]), motorPinSet, motorPinSet + 4);
+      break;
+  }
+}
+
+//----------------------------------------------------------------------------
+// splitString
+// Splits a string into seperate parameters in a 2D jagged array
+//----------------------------------------------------------------------------
+char[][] splitString(char[] string, char splitChar){
+  int splitCount = 0;       // How many times the string has been split so far
+  int splitPosOld = 0;      // The last character that the string was split at
+  int stringSize = sizeof(string);
+  // Memory is sparse; get an initial count
+  toReturn = new char[getCharCount() + 1][];
+  // Check every single character
+  for (int k = 0; k < stringSize){
+    if (string[k] == splitChar){
+      // Split the string, trim the array, move on.
+      toReturn[splitCount] = new char[k-splitPosOld];
+      for (int j = splitPosOld; j < k; j++){
+        toReturn[splitCount][j - splitPosOld] = string[j];
+      }
+    }
+  }
+  // Return our modified array
+  return toReturn;
+}
+//----------------------------------------------------------------------------
+// getCharCount
+// Counts how many times a character appears
+//----------------------------------------------------------------------------
+int getCharCount(char[] string, char toFind){
+  int stringSize = sizeof(string);
+  int count = 0;
+  for (int k = 0; k < stringSize; k++){
+    if (string[k] == toFind){
+      count++;
+    }
+  }
+  return count;
+}
+//----------------------------------------------------------------------------
+// writeMotorSpeed
+// Enables the motor.
+//----------------------------------------------------------------------------
+void writeMotorSpeed(float speed, int pwm_pin, int gpio_pin) {
   if (speed >= 0) {
     digitalWrite(gpio_pin, 0);
     analogWrite(pwm_pin, speed * 255);
@@ -47,4 +108,3 @@ void write_motor_speed(float speed, int pwm_pin, int gpio_pin) {
     analogWrite(pwm_pin, (1 + speed) * 255);
   }
 }
-
