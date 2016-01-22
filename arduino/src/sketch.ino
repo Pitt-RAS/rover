ea4435408dabb5efcee2518ba06d5e0ab2a3454a
Aaron Miller <acmiller273@gmail.com>
2016-01-21 22:55:39 -0500

    Added pin numbers and battery voltage measurement

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -63,14 +63,14 @@
 #define STRICT_PINS true
 
 // Motor pins
-#define MOTOR_FL_PWM_PIN 0
-#define MOTOR_FR_PWM_PIN 0
-#define MOTOR_BL_PWM_PIN 0
-#define MOTOR_BR_PWM_PIN 0
-#define MOTOR_FL_GPIO_PIN 0
-#define MOTOR_FR_GPIO_PIN 0
-#define MOTOR_BL_GPIO_PIN 0
-#define MOTOR_BR_GPIO_PIN 0
+#define MOTOR_FL_PWM_PIN 5
+#define MOTOR_FR_PWM_PIN 11
+#define MOTOR_BL_PWM_PIN 9
+#define MOTOR_BR_PWM_PIN 10
+#define MOTOR_FL_GPIO_PIN 2
+#define MOTOR_FR_GPIO_PIN 3
+#define MOTOR_BL_GPIO_PIN 4
+#define MOTOR_BR_GPIO_PIN 8
 
 // Buffer for serial input
 char inputBuffer[INPUT_BUFFER_SIZE];
@@ -97,30 +97,48 @@ byte motorPins[] = {
 // Analog pins, for easy access in getPin
 byte aPins[] = {A0,A1,A2,A3,A4,A5};
 
+// Battery voltage flag
+volatile bool battery_interrupt_fired = false;
+volatile bool battery_low = false;
+
 //----------------------------------------------------------------------------
 // setup
 // Runs once when the program starts
 //----------------------------------------------------------------------------
-void setup(){
+void setup() {
   // Start Serial
   Serial.begin(115200);
   Serial.setTimeout(10);
   // Set the pin mode for each motor pin, and mark it for future reference
-  for (int k = 0; k < 8; k++){
+  for (int k = 0; k < 8; k++) {
     pinModes[motorPins[k]] = OUTPUT + 1;
     Serial.print("Can we even get inside the setup?");  
   pinMode(motorPins[k], OUTPUT);
   }
+
+  // Enable analog comparator for battery voltage
+  ACSR |= _BV(ACIE) | _BV(ACIS1);
 }
 
 //----------------------------------------------------------------------------
 // loop
 // Main program loop
 //----------------------------------------------------------------------------
-void loop(){
-  byte newChars = Serial.readBytesUntil('\n', inputBuffer + inputBufferSize, INPUT_BUFFER_SIZE - inputBufferSize);
+void loop() {
+  // Send pending responses
+  if (battery_interrupt_fired) {
+    battery_interrupt_fired = false;
+
+    // Disable battery compare interrupt
+    ACSR &= ~_BV(ACIE);
+
+    Serial.print("b000000000:");
+  }
+
+  // Accept new commands
+  byte newChars = Serial.readBytes(inputBuffer + inputBufferSize, INPUT_BUFFER_SIZE - inputBufferSize);
   inputBufferSize += newChars;
-  while (inputBufferSize > 10 && inputBuffer[inputBufferSize - 1] == COMMAND_SEPERATOR && commandsSize < COMMAND_BUFFER_SIZE){
+  while (inputBufferSize > 10 && inputBuffer[inputBufferSize - 1] == COMMAND_SEPERATOR && commandsSize < COMMAND_BUFFER_SIZE) {
     // Add to be executed
     Serial.print("Reached the point where commands can actually be parsed (memcpy(commands[commandsSize] etc.))");
     memcpy(commands[commandsSize], &inputBuffer[inputBufferSize - 11], 10);
@@ -128,25 +146,25 @@ void loop(){
     commandsSize++;
   }
   // Execute the commands FIFO.
-  if (commandsSize > 0){
+  if (commandsSize > 0) {
     Serial.print("We have now started executing the commands FIFO.");
-    for (int k = commandsSize - 1; k >= 0; k--){
-      char args[] = {commands[k][1],commands[k][2]};
+    for (int k = commandsSize - 1; k >= 0; k--) {
+      char args[] = {commands[k][1], commands[k][2]};
       processCommand(commands[k][0], atof(commands[k] + 3), args);
     }
     commandsSize = 0;
   }
   // Do some clean-up here if too much stuff
-  if (inputBufferSize >= INPUT_BUFFER_SIZE * 0.5){
-     Serial.print("Now we're cleaning things up because there's too much stuff. ");
-     for (int k = inputBufferSize; k > 0; k--){
-       if (inputBuffer[k] != COMMAND_SEPERATOR){
-         inputBuffer[k] = 0;
-         inputBufferSize--;
-       }else{
-         break;
-       }
-     }
+  if (inputBufferSize >= INPUT_BUFFER_SIZE * 0.5) {
+    Serial.print("Now we're cleaning things up because there's too much stuff. ");
+    for (int k = inputBufferSize; k > 0; k--) {
+      if (inputBuffer[k] != COMMAND_SEPERATOR) {
+        inputBuffer[k] = 0;
+        inputBufferSize--;
+      } else {
+        break;
+      }
+    }
   }
 }
 
@@ -154,8 +172,8 @@ void loop(){
 // processCommand
 // Switch off to the different specified functions
 //----------------------------------------------------------------------------
-void processCommand(char cmd, float arg3_f, char* args){
-  switch (cmd){
+void processCommand(char cmd, float arg3_f, char* args) {
+  switch (cmd) {
     case 'm' : // Motor Speed Command
       runMotorCommand(arg3_f, args[0], args[1]);
       break;
@@ -196,8 +214,8 @@ void runMotorCommand(float mspeed, char arg1, char arg2) {
   // the conditions for each wheel are met above to enable it.
   // Ex: If shouldSetMotors[0] (front) and shouldSetMotors[2] (left),
   //     then apply the speed to the pin MOTOR_FL_PWM_PIN
-  for (int k = 0; k < 4; k++){
-    if (shouldSetMotors[k/2] && shouldSetMotors[2+k%2]){
+  for (int k = 0; k < 4; k++) {
+    if (shouldSetMotors[k/2] && shouldSetMotors[2+k%2]) {
       writeMotorSpeed(mspeed, motorPins[k], motorPins[k+4]);
     }
   }
@@ -207,10 +225,10 @@ void runMotorCommand(float mspeed, char arg1, char arg2) {
 // Changes the pin type as needed.
 // Provides an error if pin has already been decided on
 //----------------------------------------------------------------------------
-boolean applyPinType(byte pinID, byte type){
-  if (pinModes[pinID] == type + 1){
+boolean applyPinType(byte pinID, byte type) {
+  if (pinModes[pinID] == type + 1) {
     return true;
-  }else if (pinModes[pinID] == 0 || !STRICT_PINS){
+  }else if (pinModes[pinID] == 0 || !STRICT_PINS) {
     pinModes[pinID] = type + 1;
     pinMode(pinID, type);
     return true;
@@ -227,8 +245,8 @@ boolean applyPinType(byte pinID, byte type){
 // Gets the pin number from a set of characters.
 // Works for pins labeled A and such as well.
 //----------------------------------------------------------------------------
-byte getPin(char pin[]){
-  if (pin[0] == 'A'){
+byte getPin(char pin[]) {
+  if (pin[0] == 'A') {
     return aPins[atoi(pin + 1)]; 
   }
   return atoi(pin);
@@ -252,7 +270,7 @@ void writeMotorSpeed(float mspeed, byte pwm_pin, byte gpio_pin) {
 //----------------------------------------------------------------------------
 void readPinDigital(byte pinID) {
   // Check if pin type set to read
-  if (applyPinType(pinID,INPUT)){
+  if (applyPinType(pinID,INPUT)) {
     // Read the pin
     Serial.println(digitalRead(pinID));
   }
@@ -263,7 +281,7 @@ void readPinDigital(byte pinID) {
 //----------------------------------------------------------------------------
 void writePinDigital(byte pinID, int output) {
   // Check if pin type set to write
-  if (applyPinType(pinID,OUTPUT)){
+  if (applyPinType(pinID,OUTPUT)) {
     // Write to the pin
     digitalWrite(pinID,output);
   }
@@ -274,7 +292,7 @@ void writePinDigital(byte pinID, int output) {
 //----------------------------------------------------------------------------
 void readPinAnalog(byte pinID) {
   // Check if pin type set to read
-  if (applyPinType(pinID,INPUT)){
+  if (applyPinType(pinID,INPUT)) {
     // Read the pin
     Serial.println(analogRead(pinID));
   }
@@ -288,7 +306,7 @@ void readPinAnalog(byte pinID) {
 void writePinAnalog(byte pinID, int output) {
   // When writing, you do not need to set the pin type to output,
   // However, strict mode should prevent us from doing anything
-  if (applyPinType(pinID,OUTPUT)){
+  if (applyPinType(pinID,OUTPUT)) {
     // Write to the pin
     analogWrite(pinID,output);
   }
@@ -297,20 +315,29 @@ void writePinAnalog(byte pinID, int output) {
 // playTone
 // Plays a tone at a specified frequency on a pin
 //----------------------------------------------------------------------------
-void playTone(byte pinID, int freq){
+void playTone(byte pinID, int freq) {
   // Stop the tone as needed
-  if (tonePin != 0){
+  if (tonePin != 0) {
     noTone(tonePin);
     tonePin = 0;
   }
-  if (pinID == 0){
+  if (pinID == 0) {
     return;
   }
   // Start the new tone, with appropriate duration
   tonePin = pinID;
-  if (toneDuration == 0){
+  if (toneDuration == 0) {
     tone(pinID,freq); 
   }else{
     tone(pinID,freq,toneDuration);
   }
+}
+
+//----------------------------------------------------------------------------
+// battery voltage interrupt
+// Triggered when the battery voltage dips below the voltage threshold
+//----------------------------------------------------------------------------
+ISR(ANALOG_COMP_vect) {
+  battery_interrupt_fired = true;
+  battery_low = true;
 }
