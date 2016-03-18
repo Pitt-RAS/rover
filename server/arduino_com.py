3ef541c7bb01ccfb077b09c51d4d555c4f6544ec
Rover <robotsdonthaveemail>
2016-03-18 01:14:44 +0000

    Create python module for arduino communications

---
+++ server/arduino_com.py
@@ -0,0 +1,37 @@
+import serial
+
+#-----------------------------------------------------
+# set_camera_servo_position
+# Sets the camera servo to a specified angle
+#-----------------------------------------------------
+
+def v_servo_write(position):
+    #print("updating servo1: " + str(position))
+    arduino_serial.write('sv' + 'v') # extra v is dummy character
+    #The offset is because 120 degrees is actually 0 its constrained on the arduino
+    for b in struct.pack('f', position):
+        arduino_serial.write(b)
+    arduino_serial.write(':')
+    
+
+def h_servo_write(position):
+    #print("updating servo2: " + str(position))
+    arduino_serial.write('sh' + 'h') #extra h is dummy character
+    #The offset is because 150 degrees is actually 0 its constrained on the arduino
+    for b in struct.pack('f', position):
+        arduino_serial.write(b)
+    arduino_serial.write(':')
+    
+    
+def controlMotors(wheels, throttle):
+    arduino_serial.write('mal')
+                        for b in struct.pack('f', wheels[0] * throttle):
+                            arduino_serial.write(b)
+                        arduino_serial.write(':')
+                        arduino_serial.write('mar')
+                        for b in struct.pack('f', wheels[1] * throttle):
+                            arduino_serial.write(b)
+                        arduino_serial.write(':')
+
+
+arduino_serial = serial.Serial('/dev/ttyAMA0', 115200);
